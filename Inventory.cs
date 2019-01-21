cd8ae9ceece8560d1e3fe0244a75b9e12692ff5b
ttresori <tresoriertom@gmail.com>
2019-01-21 11:34:32 +0100

    Add files via upload

---
+++ Inventory.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public GameObject slot1 = null;
+    public GameObject slot2 = null;
+    public GameObject slot3 = null;
+    public GameObject slot4 = null;
+    public GameObject slot5 = null;
+    public GameObject slot6 = null;
+    public GameObject slot7 = null;
+    public GameObject slot8 = null;
+    public GameObject slot9 = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ PrefabItem.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabItem : MonoBehaviour
+{
+    public GameObject Prefabitem;
+    public GameObject Canvas_Player_Inventory;
+}
