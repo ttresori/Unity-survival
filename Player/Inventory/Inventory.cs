a5670ac2ad34b7a19504f2b5292d45e20333d787
root <root@DESKTOP-GLVAG5K.localdomain>
2019-02-07 19:00:57 +0100

    inventory

---
+++ Player/Inventory/Inventory.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class  Inventory : MonoBehaviour
+{
+    public GameObject[] inventory_slots = new GameObject[9];
+    private GameObject to_instantiate;
+    private Vector3 pos_to_add;
+    private Quaternion rot_to_add;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int i;
+
+        i = 0;
+        while (i < 9)
+        {
+            inventory_slots[i] = null;
+            i++;
+        }
+        to_instantiate = null;
+    }
+
+    int check_item_inventory(string to_find)
+    {
+        int i;
+
+        i = 0;
+        while (i < 9 && inventory_slots[i])
+        {
+            if (inventory_slots[i].name == to_find)
+                return (i);
+            i++;
+        }
+        return (-1);
+    }
+
+    void spawn_light()
+    {
+        to_instantiate = Instantiate(inventory_slots[check_item_inventory("Lighter")], transform.Find("MainCamera"));
+        to_instantiate.transform.position = GameObject.Find("Player").transform.position;
+        to_instantiate.transform.rotation = GameObject.Find("Player").transform.rotation;
+        pos_to_add = to_instantiate.transform.position;
+        pos_to_add.z += 1.37f;
+        to_instantiate.transform.position = pos_to_add;
+        rot_to_add = to_instantiate.transform.rotation;
+        to_instantiate.transform.rotation = rot_to_add;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("f"))
+        {
+            if (check_item_inventory("Lighter") > -1)
+            {
+                Debug.Log("Lighet is find in inventory_slot");
+                spawn_light();
+            }
+        }
+    }
+}
---
+++ Player/PickUp/PickAnItem.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickAnItem : MonoBehaviour
+{
+    public bool enter = true;
+    public bool stay = true;
+    public bool exit = true;
+    public string TagCollision;
+    public GameObject Slot1;
+    Inventory  player_script_inventory;
+   // public GameObject sprite_item;
+    PickAnItem pick_an_item;
+    GameObject slot_inv_to_modify;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (enter)
+        {
+            //Debug.Log("entered");
+        }
+    }
+
+    private void pick_item(GameObject player)
+    {
+        player_script_inventory = player.GetComponent<Inventory>();
+        pick_an_item = player.GetComponent<PickAnItem>();
+        if (player_script_inventory.inventory_slots[0] == null)
+        {
+            player_script_inventory.inventory_slots[0] = gameObject.GetComponent<PrefabItem>().Prefabitem;
+            gameObject.GetComponent<PrefabItem>().Canvas_Player_Inventory.GetComponent<Image>().sprite = gameObject.GetComponent<PrefabItem>().image;
+            //ot_inv_to_modify = pick_an_item
+            // slot_inv_to_modify = pick_an_item.sprite_item;
+           // slot_inv_to_modify =
+
+            return;
+        }
+      /*  else if (player_script_inventory.slot2 == null)
+        {
+            return;
+        }
+        else if(player_script_inventory.slot3 == null)
+        {
+            return;
+        }
+        else if (player_script_inventory.slot4 == null)
+        {
+            return;
+        }
+        else if (player_script_inventory.slot5 == null)
+        {
+            return;
+        }
+        else if (player_script_inventory.slot6 == null)
+        {
+            return;
+        }
+        else if (player_script_inventory.slot7 == null)
+        {
+            return;
+        }
+        else if (player_script_inventory.slot8 == null)
+        {
+            return;
+        }
+        else if (player_script_inventory.slot9 == null)
+        {
+            return;
+        }*/
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (stay)
+        {
+            //  Debug.Log("stay");
+            if (other.gameObject.tag == TagCollision)
+            {
+                if (Input.GetButton("Fire1"))
+                {
+                    pick_item(other.gameObject);
+                    GameObject.Destroy(gameObject);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (exit)
+        {
+         //   Debug.Log("exit");
+        }
+    }
+}
